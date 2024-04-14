c6c8b956b302daf39bd9c39ee38b4188e26da2c9
unknown <nguyenthitrang.ttd@gmail.com>
2024-04-15 00:26:40 +0700

    add use callback

--- react-tutorial/src/App.jsx
+++ react-tutorial/src/App.jsx
@@ -1,42 +1,14 @@
-import { useCallback, useMemo, useState } from 'react';
-import './App.scss';
-import Card from './components/Card/Card';
-import Card2 from './components/Card2/Card2';
+import './App.scss'
+import ReactMemo from './components/ReactMemo/ReactMemo'
+import UseMemo from './components/UseMemo/UseMemo'
 import Counter from './components/Counter/Counter'
-import Profile from './components/Profile/Profile';
-import Header from './components/Header/Header';
-import Banner from './components/Banner/Banner';
-import UserCardList from './components/UserCardList/UserCardList';
-import dataUsers from './data/users.json';
-import Footer from './components/Footer/Footer';
-import Search from './components/Search/Search';
-import UseRef from './components/UseRef/UseRef';
-import UseMemo from './components/UseMemo/UseMemo';
-import Memo from './components/Memo/Memo';
-
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [count1, setCount1] = useState(0);
-  const increment = () => {
-    setCount(pre => pre + 1)
-  }
   return (
-    <div className="app flex justify-center items-center" >
-      {/* <Card />
-      <Counter /> */}
-      {/* <Header />
-      <Banner /> */}
-      {/* <Profile /> */}
-      {/* <UserCardList usersList={dataUsers} >
-        <Search />
-      </UserCardList>
-      <Footer /> */}
-      {/* <Card2 /> */}
-      {/* <UseRef/> */}
-      {/* <UseMemo/> */}
-      <Memo onIncrement={increment} />
-      <h1>{count}</h1>
+    <div className='app flex justify-center items-center'>
+      {/* <UseRef /> */}
+      {/* <UseMemo /> */}
+      <Counter/>
     </div>
   )
 }
