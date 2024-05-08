1a4cd4f040d8356389e15671545411984a91a46c
unknown <nguyenthitrang.ttd@gmail.com>
2024-05-08 13:09:11 +0700

    add exercise 10

---
+++ exercises/Exercise-10/README.md
@@ -0,0 +1,19 @@
+## Bài tập về nhà buổi 9
+
+#### 1. Ôn tập:
+
+- [x] useContext
+- [x] useReducer
+
+#### 2. Yêu cầu tìm hiểu thêm về:
+
+- [x] redux
+
+#### 3. Bài tập về nhà
+
+- Dùng formik để hoàn thành 2 bài tập sau:
+- [x] Bài 1: Thêm tính năng chia cho bài thực hành trên lớp buổi trước, với số chia nhập từ bàn phím
+- [x] Bài 2: Vào doc buổi 10 phần vd về to-do-app (cuối doc), các bé cop code và hoàn thiện chức năng update todo
+- [x] Dealine: 23h59 ngày 12/5
+- [x] Yêu cầu nộp link deploy(có thể là vercel hoặc github page) và link code trên github
+- [x] Lưu ý các bạn có thể chia nhánh branch trên GitHub để tránh việc khởi tạo lại dự án. Với mỗi BTVN là một nhánh riêng biệt.
--- react-tutorial/src/App.jsx
+++ react-tutorial/src/App.jsx
@@ -1,25 +1,52 @@
-import React, { createContext, useReducer, useState } from 'react'
-import './App.scss'
-import Content from './components/Content/Content'
+import React, { useReducer, useState } from 'react'
+import './App.scss';
 
+// useState
+// initState = 0
+// actions (UP/Down);
 
-export const ThemeContext = createContext();
+// useReducer
+// initState 
+const initialState = 0;
 
+// actions (UP/Down);
+const UP_COUNT = 'up';
+const DOWN_COUNT = 'down';
+const NHAN_COUNT= 'nhan';
+
+// reducer
+const reducer = (state, action) => {
+  switch(action)
+  {
+    case UP_COUNT: 
+      return state + 1;
+
+    case DOWN_COUNT:
+      return state -1;
+
+    case NHAN_COUNT:
+      return state *state;
+
+    
+    
+    default:
+        throw new Error("Invalid action")
+  }
+
+}
+// dispatch
 
 function App() {
-  const [theme, setTheme] = useState(false);
-  const handleToggleTheme = () => setTheme(!theme);
-
+  const [count, dispatch] = useReducer(reducer, initialState);
+  
   return (
-    <ThemeContext.Provider value={{
-      theme,
-      handleToggleTheme
-    }}>
-      <div className='app'>
-        
-        <Content/>
-      </div>
-    </ThemeContext.Provider>
+    <div>
+      <h1>{count}</h1>
+      <button type="button" onClick={() => dispatch(UP_COUNT)}>up</button>
+      <button type="button" onClick={() => dispatch(DOWN_COUNT)}>down</button>
+      <button type="button" onClick={() => dispatch(NHAN_COUNT)}>NHAN</button>
+
+    </div>
   )
 }
 
